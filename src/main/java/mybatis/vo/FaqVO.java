8d54976e97936056832a559cc9177e9ef6606c5d
Graz1e <fprhelwkdlsj@naver.com>
2025-01-22 12:34:21 +0900

    고객지원 페이지

---
+++ src/main/java/mybatis/vo/FaqVO.java
@@ -0,0 +1,45 @@
+package mybatis.vo;
+
+public class FaqVO {
+  private String idx, title, content, reg_date, status;
+
+  public String getIdx() {
+    return idx;
+  }
+
+  public void setIdx(String idx) {
+    this.idx = idx;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public String getReg_date() {
+    return reg_date;
+  }
+
+  public void setReg_date(String reg_date) {
+    this.reg_date = reg_date;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+}
