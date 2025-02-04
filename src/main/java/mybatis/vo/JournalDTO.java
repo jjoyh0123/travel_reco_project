167d3e6629453ac4a4cdf8fde63b44b239070c81
Graz1e <fprhelwkdlsj@naver.com>
2025-02-04 11:47:43 +0900

    tmp

---
+++ src/main/java/mybatis/vo/JournalDTO.java
@@ -0,0 +1,135 @@
+package mybatis.vo;
+
+public class JournalDTO {
+  private String date_idx, visit_order, content_id, content_type_id,
+      title, thumnail, map_x, map_y, time, idx, plan_idx, date,
+       area_code, start_date, end_date, status;
+
+  public String getDate_idx() {
+    return date_idx;
+  }
+
+  public void setDate_idx(String date_idx) {
+    this.date_idx = date_idx;
+  }
+
+  public String getVisit_order() {
+    return visit_order;
+  }
+
+  public void setVisit_order(String visit_order) {
+    this.visit_order = visit_order;
+  }
+
+  public String getContent_id() {
+    return content_id;
+  }
+
+  public void setContent_id(String content_id) {
+    this.content_id = content_id;
+  }
+
+  public String getContent_type_id() {
+    return content_type_id;
+  }
+
+  public void setContent_type_id(String content_type_id) {
+    this.content_type_id = content_type_id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getThumnail() {
+    return thumnail;
+  }
+
+  public void setThumnail(String thumnail) {
+    this.thumnail = thumnail;
+  }
+
+  public String getMap_x() {
+    return map_x;
+  }
+
+  public void setMap_x(String map_x) {
+    this.map_x = map_x;
+  }
+
+  public String getMap_y() {
+    return map_y;
+  }
+
+  public void setMap_y(String map_y) {
+    this.map_y = map_y;
+  }
+
+  public String getTime() {
+    return time;
+  }
+
+  public void setTime(String time) {
+    this.time = time;
+  }
+
+  public String getIdx() {
+    return idx;
+  }
+
+  public void setIdx(String idx) {
+    this.idx = idx;
+  }
+
+  public String getPlan_idx() {
+    return plan_idx;
+  }
+
+  public void setPlan_idx(String plan_idx) {
+    this.plan_idx = plan_idx;
+  }
+
+  public String getDate() {
+    return date;
+  }
+
+  public void setDate(String date) {
+    this.date = date;
+  }
+
+  public String getArea_code() {
+    return area_code;
+  }
+
+  public void setArea_code(String area_code) {
+    this.area_code = area_code;
+  }
+
+  public String getStart_date() {
+    return start_date;
+  }
+
+  public void setStart_date(String start_date) {
+    this.start_date = start_date;
+  }
+
+  public String getEnd_date() {
+    return end_date;
+  }
+
+  public void setEnd_date(String end_date) {
+    this.end_date = end_date;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+}
